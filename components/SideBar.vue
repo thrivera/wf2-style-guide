6e9fc328c189913dc43883751cbfb536d6bae81c
Justin Chiou <justin@thrivera.io>
2018-05-17 17:47:17 -0400

    componentize the sidebar

---
+++ components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <aside class="flex-wrapper">
+    <div class="side-bar">
+      <nuxt-link to="/">
+        <img class="logo" src="/logo.png" alt="Wunderfund">
+      </nuxt-link>
+    </div>
+    <nav class="navigation">
+      <subtitle>Style Guide</subtitle>
+      <ul>
+        <li><nuxt-link class="nav-link" to="/">Colors</nuxt-link></li>
+        <li><nuxt-link class="nav-link" to="/fonts">Fonts</nuxt-link></li>
+        <li><nuxt-link class="nav-link" to="/logos">Logos</nuxt-link></li>
+        <li><nuxt-link class="nav-link" to="/ui-elements">UI Elements</nuxt-link></li>
+        <li><nuxt-link class="nav-link" to="/icons">Icons</nuxt-link></li>
+        <li><nuxt-link class="nav-link" to="/forms">Forms</nuxt-link></li>
+        <li><nuxt-link class="nav-link" to="/media">Media</nuxt-link></li>
+      </ul>
+    </nav>
+  </aside>
+</template>
